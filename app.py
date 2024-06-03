702f012ce7e410a2ca573912ad0c91c23599ce7b
laksa-ajaa <laksmanachairutama@gmail.com>
2024-06-04 00:52:27 +0700

    add: user visitor page

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def showHome():
+    return render_template('user_page/index.html')
+
+@app.route('/sejarah')
+def showSejarah():
+    return render_template('user_page/sejarah.html')
+
+@app.route('/kontak')
+def showKontak():
+    return render_template('user_page/kontak.html')
+
+@app.route('/visimisi')
+def showVisiMisi():
+    return render_template('user_page/visimisi.html')
+
+@app.route('/kegiatan')
+def showKegiatan():
+    return render_template('user_page/kegiatan.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
