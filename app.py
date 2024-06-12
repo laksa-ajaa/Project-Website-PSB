bf378908bfe7b7a9b5513d35c2e6e9cd3c8a637a
rgardhar <rgardha@gmail.com>
2024-06-13 01:22:37 +0700

    backend login admin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, redirect, url_for, render_template, request, jsonify, send_from_directory, send_file, make_response
+
 
 app = Flask(__name__)
 
@@ -16,7 +17,61 @@ def auth():
     }
     return render_template('auth/login.html', data=data)
 
+#route login admin
+@app.route('/loginAdmin')
+def login_admin():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+    pw_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
 
+    result = db.admin.find_one({"email": email, "password": pw_hash})
+
+    if result:
+        payload = {
+            "email": email,
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+        response = make_response(
+            jsonify({
+                "message": "success",
+                "email": email,
+                "token": token
+            })
+        )
+        response.set_cookie("token", token)
+        return response
+
+    else:
+        return jsonify({
+            "message": "fail",
+            "error": "We could not find a user with that email/password combination"
+        })
+    
+def adminTokenAuth(view_func):
+    @wraps(view_func)
+    def decorator(*args, **kwargs):
+        token_receive = request.cookies.get("token")
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            email = payload.get('email')
+            admin_info = db.pend_santri.admin.find_one({'email': email})
+            if admin_info:
+                admin = [admin_info]
+                return view_func(*args, admin=admin, **kwargs)
+            else:
+                return redirect(url_for('indexAdmin'))
+        except jwt.ExpiredSignatureError:
+            msg = 'Your token has expired'
+            return redirect(url_for('indexAdmin', msg=msg))
+        except jwt.exceptions.DecodeError:
+            print("Received token:", token_receive)
+            msg = 'There was a problem logging you in'
+            return redirect(url_for('indexAdmin', msg=msg))
+
+    return decorator
 
 @app.route('/sejarah')
 def showSejarah():
